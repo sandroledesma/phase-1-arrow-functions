5a99de059031a3a35f6bd977a97da0aaa595dbba
Alessandro <sandroledesma@gmail.com>
2024-04-27 23:47:55 -0400

    add files

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// has a function expression called divide
+// divides 2000 by 100
+function divide() {
+    let a = 2000;
+    let b = 100;
+    return divide = a / b;
+}
+
+// has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+let x = 2;
+const square = (x => x * x);
+
+//has an arrow function called add
+//add arrow function takes two parameters and adds them together
+let a = 3
+let b = 4
+const add = (a, b) => a + b;
